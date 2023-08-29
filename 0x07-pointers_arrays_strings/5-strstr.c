7813d6cd5c0cda175bb38ade9aa474fd47ee819c
Raghdaaseyam <Raghdaa.seyam@gmail.com>
2023-08-29 18:46:27 +0300

    error fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * rev_string -0 reverse the string
- * @s: pointer to string.
- *
- * Return: void
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
  */
-void rev_string(char *s)
+char *_strstr(char *haystack, char *needle)
 {
-	int i, j, k, temp;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
 	}
-	k = 0;
-	j = i - 1;
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
-	}
+
+	return (0);
 }
